5bc788a4c10e3510d011c55fab0d9de5c8acbc8a
Pedrassolli <pedropedrassolli@outlook.com>
2021-07-20 10:39:50 -0300

    build(bootstrap): install bootstrap, jquery and popper.js using yarn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
+
+import bootstrap
 
 Rails.start()
 Turbolinks.start()
