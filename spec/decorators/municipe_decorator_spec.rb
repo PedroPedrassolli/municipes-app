859f675350888c00d349fa0cba3fc3ec4c6aac0b
Pedrassolli <pedropedrassolli@outlook.com>
2021-08-02 11:30:55 -0300

    test(municipe - municipe_decorator): adds tests for both
    also adds gems for the testing, test images and factories

--- spec/decorators/municipe_decorator_spec.rb
+++ spec/decorators/municipe_decorator_spec.rb
@@ -3,4 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe MunicipeDecorator do
+  let(:municipe) { build(:municipe) }
+
+  it { expect(municipe.decorate.birthdate).to eq I18n.l(municipe.birthdate) }
+  it { expect(municipe.decorate.cpf).to eq CPF.new(municipe.cpf).formatted }
+  it { expect(municipe.decorate.phone).to eq Phonelib.parse(municipe.phone).national }
+  it { expect(municipe.decorate.status).to eq I18n.t('activerecord.attributes.municipe.status.active') }
 end
